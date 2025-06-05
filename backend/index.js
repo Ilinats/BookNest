9864f9df00000671f34193dc6e39150f3e967ad3
Ilina Tsanova <107075825+Ilinats@users.noreply.github.com>
2025-06-06 00:38:10 +0300

    cors index js update

--- backend/index.js
+++ backend/index.js
@@ -20,17 +20,17 @@ const libraryEntryRoutes = require('./routes/libraryEntryRoutes');
 const authenticateRoutes = require('./routes/auth.js');
 
 const app = express();
-app.options('*', cors()); // handle preflight requests
-app.use(cors()); // Allow CORS
+// app.options('*', cors()); // handle preflight requests
+// app.use(cors()); // Allow CORS
 const prisma = new PrismaClient();
 const PORT = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // Allow all origins
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(express.json());
 
